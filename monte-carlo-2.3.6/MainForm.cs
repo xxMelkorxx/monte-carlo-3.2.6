74a5cf602524d628d4952599af83b5c2dca20f0e
SerBor <xxxserborxxx@gmail.com>
2022-11-30 19:20:55 +0300

    feat: create solution

---
+++ monte-carlo-2.3.6/MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace monte_carlo_2._3._6
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
